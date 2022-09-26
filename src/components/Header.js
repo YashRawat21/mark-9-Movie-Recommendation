e4d2680fa660a34676b00da4b71b20ca6912cc22
YashRawat21 <rawat17yash@gmail.com>
2022-09-26 13:45:17 +0530

    updated

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export const Header = () => {
+  return (
+    <div>
+       <h1>🎬 Movie Recommendation🍿</h1> 
+    <h3>Action</h3> 
+    <h3>Romantic</h3> 
+    <h3>Horror</h3> 
+    <h3>Marvel Superheroes</h3> 
+    </div>
+  )
+}
